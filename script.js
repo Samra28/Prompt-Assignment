bcb8a685b349361979b3a1de1b93b690be6d7008
Samra28 <154986251+Samra28@users.noreply.github.com>
2024-03-11 16:36:50 +0500

    Add files via upload
    prompt Assignment

---
+++ script.js
@@ -0,0 +1,11 @@
+let name = prompt("What is your first name")
+
+console.log(name)
+
+let yes = prompt("What is your middle name")
+
+console.log(yes)
+
+let mid = prompt("What is your last name")
+
+console.log(mid)
